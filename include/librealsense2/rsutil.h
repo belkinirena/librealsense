4aabebf343477cc879bab87ec7b5b36b0b57b5e3
Belkin <irena.belkin@intel.com>
2018-08-15 18:07:52 +0300

    WIP

--- include/librealsense2/rsutil.h
+++ include/librealsense2/rsutil.h
@@ -135,8 +135,6 @@ static void rs2_project_color_pixel_to_depth_pixel(float to_pixel[2],
     float min_dist = -1;
     for (float p[2] = { start_pixel[0], start_pixel[1] }; is_pixel_in_line(p, start_pixel, end_pixel); next_pixel_in_line(p, start_pixel, end_pixel))
     {
-        (get_frame_data())[y*get_width() + x]
-
         float depth = rs2_depth_frame_get_distance(depth_frame, p[0], p[1], 0);
         if (depth == 0) 
             continue;
