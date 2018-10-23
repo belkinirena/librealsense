98a2b32eae889dc108eea978a493c691a6dac324
Belkin <irena.belkin@intel.com>
2018-10-23 14:26:20 +0300

    Run all tests and print result

--- unit-tests/unit-tests-main.cpp
+++ unit-tests/unit-tests-main.cpp
@@ -48,6 +48,7 @@ int main(int argc, char* const argv[])
             std::cout << "Didn't run any tests!\n";
             return EXIT_FAILURE;
         }
+        std::cout << "Result : " << result << std::endl;
         return result;
     }
     catch (...)
