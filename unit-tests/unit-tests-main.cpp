8f16377f1e99ea68989c9868ebea34592d45d765
Belkin <irena.belkin@intel.com>
2018-10-23 13:52:37 +0300

    Add try catch

--- unit-tests/unit-tests-main.cpp
+++ unit-tests/unit-tests-main.cpp
@@ -5,48 +5,53 @@
 
 int main(int argc, char* const argv[])
 {
+    try {
+        command_line_params::instance(argc, argv);
 
-    command_line_params::instance(argc, argv);
+        std::vector<char*> new_argvs;
 
-    std::vector<char*> new_argvs;
-
-    std::cout << "Running tests with the following parameters: ";
-    for (auto i = 0; i < argc; i++)
-    {
-        std::string param(argv[i]);
-        std::cout << param << " ";
-    }
-    std::cout << std::endl;
-
-    for (auto i = 0; i < argc; i++)
-    {
-        std::string param(argv[i]);
-        if (param != "into" && param != "from")
+        std::cout << "Running tests with the following parameters: ";
+        for (auto i = 0; i < argc; i++)
         {
-            new_argvs.push_back(argv[i]);
+            std::string param(argv[i]);
+            std::cout << param << " ";
         }
-        else
+        std::cout << std::endl;
+
+        for (auto i = 0; i < argc; i++)
         {
-            i++;
-            if (i < argc && param == "from")
+            std::string param(argv[i]);
+            if (param != "into" && param != "from")
             {
-                auto filename = argv[i];
-                std::ifstream f(filename);
-                if (!f.good())
+                new_argvs.push_back(argv[i]);
+            }
+            else
+            {
+                i++;
+                if (i < argc && param == "from")
                 {
-                    std::cout << "Could not load " << filename << "!"  << std::endl;
-                    return EXIT_FAILURE;
+                    auto filename = argv[i];
+                    std::ifstream f(filename);
+                    if (!f.good())
+                    {
+                        std::cout << "Could not load " << filename << "!" << std::endl;
+                        return EXIT_FAILURE;
+                    }
                 }
             }
         }
+
+        auto result = Catch::Session().run(static_cast<int>(new_argvs.size()), new_argvs.data());
+
+        if (!command_line_params::instance()._found_any_section)
+        {
+            std::cout << "Didn't run any tests!\n";
+            return EXIT_FAILURE;
+        }
+        return result;
     }
-
-    auto result = Catch::Session().run(static_cast<int>(new_argvs.size()), new_argvs.data());
-
-    if(!command_line_params::instance()._found_any_section)
+    catch (...)
     {
-        std::cout << "Didn't run any tests!\n";
-        return EXIT_FAILURE;
+        std::cout << "ERROR!!!!!!!!!!!!!!\n";
     }
-    return result;
 }
