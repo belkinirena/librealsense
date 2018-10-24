c128497347ef7f2b720f27fb3b1a810b84d61e81
Belkin <irena.belkin@intel.com>
2018-10-24 09:43:39 +0300

    Replace to try_wait_for_frames

--- unit-tests/unit-tests-live.cpp
+++ unit-tests/unit-tests-live.cpp
@@ -4843,7 +4843,8 @@ TEST_CASE("Projection from recording", "[software-device][using_pipeline][projec
     while (!depth_profile || !color_profile)
     {
         WARN("1");
-        frameset frames = sync.wait_for_frames();
+        frameset frames;
+        REQUIRE(sync.try_wait_for_frames(&frames, 5000));
         REQUIRE(frames.size() > 0);
         if (frames.size() == 1)
         {
